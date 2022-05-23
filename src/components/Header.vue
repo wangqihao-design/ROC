74b82d0ad6433fa8afd251310813484dc304c421
wangqihao-design <wangqihaozuishuai@icloud.com>
2022-05-23 00:26:48 -0400

    tools to develop

---
+++ src/components/Header.vue
@@ -0,0 +1,33 @@
+<template>
+  <div style="height: 50px; line-height: 50px; border-bottom: 1px solid #CCC;display: flex">
+    <div style="width: 100px; font-weight: bold;color: dodgerblue; padding-left: 30px">Editor</div>
+    <div style="flex: 1"></div>
+    <div style="width: 200px; padding-top: 17px">
+      <el-dropdown>
+          <span class="el-dropdown-link">
+            Dropdown List<el-icon class="el-icon--right"><arrow-down /></el-icon>
+          </span>
+        <template #dropdown>
+          <el-dropdown-menu>
+            <el-dropdown-item>Action 1</el-dropdown-item>
+            <el-dropdown-item>Action 2</el-dropdown-item>
+            <el-dropdown-item>Action 3</el-dropdown-item>
+            <el-dropdown-item>Action 4</el-dropdown-item>
+            <el-dropdown-item>Action 5</el-dropdown-item>
+          </el-dropdown-menu>
+        </template>
+      </el-dropdown>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header"
+}
+</script>
+
+
+<style>
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div className="home">
+    <Header />
+    <Aiside />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+import Header from ""
+
+export default {
+  name: 'Home',
+  components: {
+  }
+}
+</script>
